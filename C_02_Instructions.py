a4f6eb88698027c13c2ab915d3e47922c3465bdb
blesera <161520579+blesera@users.noreply.github.com>
2024-03-08 11:34:43 +1300

    Add files via upload

---
+++ C_02_Instructions.py
@@ -0,0 +1,12 @@
+# Generates headings (eg: ---- Heading ----)
+
+def statement_generator(statement, decoration):
+    print(f"\n{decoration * 5} {+statement} {decoration * 5}")
+
+# Display instructions
+def instructions():
+    print("Instructions go here...")
+
+
+# Main routine goes here
+
